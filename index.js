83e1d5b13008d81fdb466debae3fae86254ae2c8
docbogdanc <docbogdanc@yahoo.com>
2024-02-19 13:18:59 +0000

    simple input text is saved as file.txt - test passed

--- index.js
+++ index.js
@@ -5,15 +5,36 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What the title of the project',
+      },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+ 
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        // Write the answers to a new file
+        const data = `Title is : ${answers.title}\nFavorite Color: ${answers.title}`;
+        fs.writeFile('file.txt', data, (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Answers saved to file.txt');
+            }
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 }
 
